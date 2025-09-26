452490acf4a62f4c2fe7eeac450723161965d3ab
Isara Kerdpra <isarafff@gmail.com>
2025-09-27 00:34:02 +0700

    add about page to run dynamic server rendering

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from "next"
+import { cookies } from "next/headers"
 import Link from "next/link"
 
 export const metadata: Metadata = {
@@ -7,11 +8,14 @@ export const metadata: Metadata = {
   }
 }
 
-export default function About() {
+export default async function About() {
+  const cookieStore = await cookies()
+  const theme = cookieStore.get("theme")
+  console.log(theme); 
   console.log("about page server component")
   return (
     <div>
-      <h1>About Us</h1>
+      <h1>About Us {new Date().toLocaleTimeString()}</h1>
       <p>We are a company dedicated to providing the best services to our customers.</p>
       <h2>
         <Link href="/">

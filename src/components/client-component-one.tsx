938aa6a378b040b72c8d2bcc68926374f984dfb2
Isara Kerdpra <isarafff@gmail.com>
2025-10-01 21:14:09 +0700

    add interleaving component

---
+++ src/components/client-component-one.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { useState } from "react";
+import { ClientComponentTwo } from "./client-component-two";
+import { ServerComponentOne } from "./server-component-one";
+
+export const ClientComponentOne = ({ children }: { 
+  children: React.ReactNode 
+}) => {
+  const [name, setName] = useState("Batman");
+
+  return (
+    <>
+      <h1>Client component one {name}</h1>
+      {children}
+      {/* <ClientComponentTwo /> */}
+    </>
+  )
+}
---
+++ src/components/client-component-two.tsx
@@ -0,0 +1,9 @@
+"use client"
+
+import { useState } from "react";
+
+export const ClientComponentTwo = () => {
+  const [name, setName] = useState("Superman");
+
+  return <h1>Client component two {name}</h1>
+}
